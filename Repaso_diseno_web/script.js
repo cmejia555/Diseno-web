bd7825fc85639b94d62aa6fd238eef6e3b2e45a8
cmejia <cesarmejia555@yahoo.com.ar>
2019-01-10 02:03:17 -0300

    Agregado de archivo javascript y funcionalidad

---
+++ Repaso_diseno_web/script.js
@@ -0,0 +1,7 @@
+document.getElementById("p2").innerHTML="JAVA SCRIPT";
+
+function func() {
+	document.getElementById("p2").innerHTML="JAVA JAVA";
+	document.getElementById("p2").style.color="red";
+	document.getElementById("p2").style.fontSize="30px"
+}
