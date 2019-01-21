52ac376fc6ccac16e5864047750a93ce0158f5bc
cmejia <cesarmejia555@yahoo.com.ar>
2019-01-21 17:17:19 -0300

    Creacion de package JSON e instalacion del modulo 'mongodb'.

--- Repaso_diseno_web/index.js
+++ Repaso_diseno_web/index.js
@@ -1,7 +1,12 @@
-var server = require("./node_modules/server.js"); // no es necesario la extension .js
-var router = require("./node_modules/router.js"); // si es necesario ./xxx
-var requestHandlers = require("./node_modules/requestHandlers.js");
+var server = require("./node_js/server.js"); // no es necesario la extension .js
+var router = require("./node_js/router.js"); // si es necesario ./xxx
+var requestHandlers = require("./node_js/requestHandlers.js");
 
+const mongoClient = require("mongodb").MongoClient;
+const assert = require('assert');
+
+//const dbName = "firstdb";
+const url = "mongodb://localhost:27017/firstdb";
 
 var handle = {} //objeto vacio
 handle["/"] = requestHandlers.sendHtml;
@@ -11,3 +16,11 @@ handle["/script.js"] = requestHandlers.sendJs;
 handle["/saveData"] = requestHandlers.save;
 
 server.init(router.route, handle);
+
+
+mongoClient.connect(url, function(err, db){
+	//if(err) throw err;
+	assert.equal(null, err);
+	console.log("Database created");
+	db.close();
+});
